f890790c66b1d1ba10c8f9296734ac757fe4696b
Mohd Saqib <mohd.saqib@vidyamantra.com>
2023-01-10 13:31:06 +0530

    difference between interface and type alias]

--- interface-vs-type.ts
+++ interface-vs-type.ts
@@ -43,16 +43,17 @@ const userT2: UserType2 = {
 // === difference between extending interface and inersecting type.
 
 // interface
+
 /*
     interface UserInterface3 extends UserInterface2 {
         age: string
     }
 
 */
-//  duplicate property with different value type gives following error.
-//  Interface 'UserInterface3' incorrectly extends interface 'UserInterface2'.
-//  Types of property 'age' are incompatible.
-//  Type 'string' is not assignable to type 'number'.
+    //  duplicate property with different value type gives following error.
+    //  Interface 'UserInterface3' incorrectly extends interface 'UserInterface2'.
+    //  Types of property 'age' are incompatible.
+    //  Type 'string' is not assignable to type 'number'.
 
 
 
@@ -71,9 +72,62 @@ type UserType3 = UserType2 & {
         age: '20'
     }
 */
+    // the code above produces following error..
+    // Type 'string' is not assignable to type 'never'.
 
-// the code above produces following error..
-// Type 'string' is not assignable to type 'never'.
+
+
+
+
+// ===== difference between interface and type
+
+// inetrface
+    // interfaces are always open to add new props to interface
+    // defining interface with same name performs merge.
+interface interface1 {
+    prop1: string
+}
+interface interface1 {
+    prop2: number
+}
+
+const user: interface1 = {
+    prop1: 'abc',
+    prop2: 2
+}
+
+// type
+    // type aliases gets closed after the initialisation
+    // we can't reinitialize type to add more properties. 
+type type1 = {
+    prop1: 'abc'
+}
+/*
+    type type1 = {
+        
+    }
+*/
+// above code produces the following error
+// Duplicate identifier 'type1'.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
