1e2bf6f56c529ca22d3fddda79ba663b2f0197d2
Mohd Saqib <mohd.saqib@vidyamantra.com>
2023-01-10 12:33:03 +0530

    extending and intersecting of interface and type

--- interface-vs-type.ts
+++ interface-vs-type.ts
@@ -17,3 +17,26 @@ type UserType = {
 const userT: UserType = {
     name: 'abc'
 }
+
+// ==== both can extend but differently
+
+// interface
+interface UserInterface2 extends UserInterface {
+    age: number
+}
+
+const userI2: UserInterface2 = {
+    name: 'xyz',
+    age: 20
+}
+
+// type
+type UserType2 = UserType & {
+    age: number
+}
+
+const userT2: UserType2 = {
+    name: 'abc',
+    age: 20
+}
+
